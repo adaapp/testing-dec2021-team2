b93fd35e56898fd865b036cda2483f8210598884
cazumara <90607633+cazumara@users.noreply.github.com>
2021-12-16 15:21:20 +0000

    Fixed tests and added RGB inputs

--- src/App.js
+++ src/App.js
@@ -2,29 +2,45 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Team 2: Colour Converter</p>
-        <form>
-          <button className="Launch-Button" onClick={Launch}>Launch Project! 🚀</button>
-        </form>
-        <a
-          className="App-link"
-          href="https://github.com/adaapp/testing-dec2021-team2"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Find our project here!
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="App">
+			<header className="App-header">
+				<img src={logo} className="App-logo" alt="logo" />
+				<p>Team 2: Colour Converter</p>
+				<form>
+					<button className="Launch-Button" onClick={Launch}>Launch Project! 🚀</button>
+				</form>
+				<a
+					className="App-link"
+					href="https://github.com/adaapp/testing-dec2021-team2"
+					target="_blank"
+					rel="noopener noreferrer"
+				>
+					Find our project here!
+				</a>
+				<form>
+				<label>
+					R:
+					<input type="number" name="r" />
+				</label>
+				<label>
+					G:
+					<input type="number" name="g" />
+				</label>
+				<label>
+					B:
+					<input type="number" name="b" />
+				</label>
+				<input type="submit" value="Submit" />
+			</form>
+			</header>
+		
+		</div>
+	);
 }
 
 function Launch() {
-  window.open("https://www.google.com/")
+	window.open("../public/index.html")
 }
 
 export default App;
