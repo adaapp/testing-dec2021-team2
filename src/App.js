0c1a02572060ea1e36af3970b3bfccf9ae07cc76
berryj21 <90607569+berryj21@users.noreply.github.com>
2021-12-16 13:18:31 +0000

    Updated Landing page
    I change the default landing page to have our project title and link to our Github

--- src/App.js
+++ src/App.js
@@ -6,16 +6,14 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <p>Team 2: Colour Converter</p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://github.com/adaapp/testing-dec2021-team2"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Find our project here!
         </a>
       </header>
     </div>
